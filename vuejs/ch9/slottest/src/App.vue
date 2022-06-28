843964b32b39d900af8645a5d011d51bf32a7fb8
mino <imsm2no@gmail.com>
2022-06-28 23:13:22 +0900

    컴포넌트 연습

---
+++ vuejs/ch9/slottest/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <speech-box :headerText="A.header" :footerText="A.footer">
+      <div>
+        <p>
+          {{A.message}}
+        </p>
+      </div>    
+    </speech-box>
+
+    <speech-box class="sanders" :headerText="B.header" :footerText="B.footer">
+      <div>
+        <p>
+          {{B.message}}
+        </p>
+      </div>    
+    </speech-box>
+  </div>
+</template>
+
+<script>
+import SpeechBox from './components/SpeechBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SpeechBox
+  },
+  data: function() {
+    return {
+      A: {
+        header: "A 헤더",
+        footer: "A 푸터",
+        message: "A 메시지",
+      },
+      B: {
+        header: "B 헤더",
+        footer: "B 푸터",
+        message: "B 메시지",
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sanders {
+  background-color: antiquewhite;
+}
+.sanders-content {
+  font-family: '굴림';
+  text-decoration: underline;
+}
+</style>
---
+++ vuejs/ch9/styletest/src/components/Child1.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: 'child1',
+    data: function() {
+        return {
+            msg: 'child1'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.main { border: solid 1px black; background-color: yellow; }
+</style>
