7e50d58af35ac82d032bd21c707bd8009e9ce8be
mino <imsm2no@gmail.com>
2022-09-05 22:19:16 +0900

    vue 라우터 연습

---
+++ vuejs/vue-news/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+
+export default {
+  components: {
+    ToolBar,
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
