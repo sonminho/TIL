172dc5d778b9ceec6a1958edaf83c891ccae70a9
mino <imsm2no@gmail.com>
2022-10-31 23:52:52 +0900

    로그인폼 유효성 검증

--- vuejs/vue-til/src/views/LoginPage.vue
+++ vuejs/vue-til/src/views/LoginPage.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    login
+    <h1>로그인 페이지</h1>
+    <LoginFrom></LoginFrom>
   </div>
 </template>
 
 <script>
+import LoginFrom from '@/components/LoginForm.vue';
 export default {
-
+  components: {
+    LoginFrom,
+  }
 }
 </script>
 
